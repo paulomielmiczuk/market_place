988cbae99b2fff09f69d3fac7f969c586b533ce5
paulo mielmiczuk <mielmiczukpaulo@gmail.com>
2024-08-29 15:08:00 -0300

    Criação do botão 'confirm' e da desabilitação quando clicado; ajuste do preço de acordo com a quantidade de dias

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,13 @@ class Booking < ApplicationRecord
   belongs_to :place
   belongs_to :user
 
-  validates :checkin, :checkout, :status, presence: true
+  validates :checkin, :checkout, presence: true
+  validates :status, inclusion: { in: [true, false] }
+
+  def total_price
+    days = (checkout - checkin).to_i
+    total_price = days * place.price
+
+    return total_price
+  end
 end
